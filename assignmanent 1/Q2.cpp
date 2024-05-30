38e994daa73d46572cd991e746b1ffc70ff04826
Aakash-GenJi <thakur.aakash569@gmail.com>
2024-05-30 21:46:29 +0545

    fixed minor inconvience

--- assignmanent 1/Q2.cpp
+++ assignmanent 1/Q2.cpp
@@ -3,17 +3,17 @@
 using namespace std;
 int num(int a)
 {
-        int i;
-        for(i=2;i<a;i++)
+     int i;
+     for(i=2;i<a;i++)
+     {
+        if(a%i==0)
         {
-            if(a%i==0)
-            {
-                cout <<a<<" is composite"<<endl;
-                return 0;
-            }
+             cout <<a<<" is composite."<<endl;
+            return 0;
         }
-        cout<<a<<" is prime."<<endl;
     }
+    cout<<a<<" is prime."<<endl;
+}
 int main()
 {
     int a;
