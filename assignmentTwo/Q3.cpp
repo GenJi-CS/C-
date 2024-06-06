18b868d334d1ea6ab722d96ff1d52beaa7a85a49
Aakash-GenJi <thakur.aakash569@gmail.com>
2024-06-07 01:35:45 +0545

    Done!

--- assignmentTwo/Q3.cpp
+++ assignmentTwo/Q3.cpp
@@ -8,3 +8,48 @@ void getDimensions(double &, double &)
 double perimeter()
 
 Write the main() function to check if your implementation works correctly.*/
+#include<iostream>
+using namespace std;
+class rect
+{
+    private:
+        int x,y,x1,y1;
+        double length,breadth;
+    
+    public:
+        void setPoints()
+        {
+            cout<<"Enter either topLeft and bottomRight or topRight and bottomLeft dimensions!"<<endl;
+            cout<<"Enter the dimesion of 1st point: ",cin>>x>>y;
+            cout<<"Enter the dimension of 2nd point: ",cin>>x1>>y1;
+        }
+        void getDimensions()
+        {
+            if((x>x1 && y>y1) || (x<x1 && y<y1))
+            {
+                if(x<x1)
+                {
+                    length=x1-x;
+                    breadth=y1-y;
+                }
+                else
+                {
+                    length=x-x1;
+                    breadth=y-y1;
+                }
+            }
+        }
+        double perimeter()
+        {
+            return 2*(length+breadth);
+        }
+};
+
+int main()
+{
+    rect R1;
+    R1.setPoints();
+    R1.getDimensions();
+    cout<<"The perimeter of the given rectangle is: "<<R1.perimeter()<<" Units."<<endl;
+    return 0;
+}
